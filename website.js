752aca97275a12037a0c46d7045eabbf20f83232
Dylan-Ay <dylan.ayache.68@gmail.com>
2022-03-01 20:33:51 +0100

    Update JS, update arrows, update CSS, update laptop-img

--- website.js
+++ website.js
@@ -1,8 +1,77 @@
 // Variables
 const burgerMenu = document.getElementById('navbar-toggler');
-const burgerIcon = document.getElementById('navbar-icon');
+const burgerIcon = document.getElementById('navbar-icon'); 
 let burgerBoolean = true;
+const arrowIcon = document.querySelector('.first-arrow');
+const arrowIconTwo = document.querySelector('.second-arrow');
+const arrowIconThree = document.querySelector('.third-arrow');
+const firstLink = document.querySelector('.first-link');
+const secondLink = document.querySelector('.second-link');
+const thirdLink = document.querySelector('.third-link');
+let arrowBoolean = true;
 
+// Change arrow position up to down
+function changeArrowPosition(){
+    firstLink.addEventListener("click", () => {
+        if (arrowBoolean === true){
+            arrowIcon.style.transform = "rotate(180deg)";
+            arrowBoolean = false;
+        } else{
+            arrowIcon.style.transform = "rotate(0deg)";
+            arrowBoolean = true;
+        }
+    });
+    secondLink.addEventListener("click", () => {
+        if (arrowBoolean === true){
+            arrowIconTwo.style.transform = "rotate(180deg)";
+            arrowBoolean = false;
+        } else{
+            arrowIconTwo.style.transform = "rotate(0deg)";
+            arrowBoolean = true;
+        }
+    });
+    thirdLink.addEventListener("click", () => {
+        if (arrowBoolean === true){
+            arrowIconThree.style.transform = "rotate(180deg)";
+            arrowBoolean = false;
+        } else{
+            arrowIconThree.style.transform = "rotate(0deg)";
+            arrowBoolean = true;
+        }
+    });
+};
+
+function resetArrowPosition(){
+    document.addEventListener('click', function(event) {
+        var isClickInside = firstLink.contains(event.target);
+        if (!isClickInside) {
+            arrowIcon.style.transform = "rotate(0deg)";
+            arrowBoolean = true;
+        }
+    });
+
+    document.addEventListener('click', function(event) {
+        var isClickInside = secondLink.contains(event.target);
+        if (!isClickInside) {
+            arrowIconTwo.style.transform = "rotate(0deg)";
+            arrowBoolean = true;
+        }
+    });
+
+    document.addEventListener('click', function(event) {
+        var isClickInside = thirdLink.contains(event.target);
+        if (!isClickInside) {
+            arrowIconThree.style.transform = "rotate(0deg)";
+            arrowBoolean = true;
+        }
+    });
+};
+
+changeArrowPosition();
+resetArrowPosition();
+
+
+// Change Burger Icon to cross
 function changeBurgerIcon(){
     burgerMenu.addEventListener("click", () => { 
         if (burgerBoolean === false){
